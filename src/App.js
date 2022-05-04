f8c0a2e4a6aaee6cab0c554f50cb3bf4a76f0252
James Luty <luty0125@gmail.com>
2022-05-04 12:26:28 -0700

    added css and className

--- src/App.js
+++ src/App.js
@@ -2,35 +2,35 @@ import './App.css';
 import PersonCard from "./components/PersonCard"
 
 function App() {
-  const peopleInfo = [
-    {
-      'firstname': 'Jane',
-      'lastname': 'Doe',
-      'age': 45,
-      'hair_color': 'Black'
-    },
-    {
-      'firstname': 'John',
-      'lastname': 'Smith',
-      'age': 88,
-      'hair_color': 'Brown'
-    },
-    {
-      'firstname': 'Millard',
-      'lastname': 'Fillmore',
-      'age': 50,
-      'hair_color': 'Brown'
-    },
-    {
-      'firstname': 'Maria',
-      'lastname': 'Smith',
-      'age': 62,
-      'hair_color': 'Brown'
-    }
-  ]
+  // const peopleInfo = [
+  //   {
+  //     'firstname': 'Jane',
+  //     'lastname': 'Doe',
+  //     'age': 45,
+  //     'hair_color': 'Black'
+  //   },
+  //   {
+  //     'firstname': 'John',
+  //     'lastname': 'Smith',
+  //     'age': 88,
+  //     'hair_color': 'Brown'
+  //   },
+  //   {
+  //     'firstname': 'Millard',
+  //     'lastname': 'Fillmore',
+  //     'age': 50,
+  //     'hair_color': 'Brown'
+  //   },
+  //   {
+  //     'firstname': 'Maria',
+  //     'lastname': 'Smith',
+  //     'age': 62,
+  //     'hair_color': 'Brown'
+  //   }
+  // ]
 
   return (
-    <div className="App">
+    <div className="Center">
       <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
       <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
       <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
