c750d87097c734a8d46d5f7775af5496df1a6924
James Luty <luty0125@gmail.com>
2022-05-04 12:23:34 -0700

    Add PersonCard component and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from "./components/PersonCard"
 
 function App() {
+  const peopleInfo = [
+    {
+      'firstname': 'Jane',
+      'lastname': 'Doe',
+      'age': 45,
+      'hair_color': 'Black'
+    },
+    {
+      'firstname': 'John',
+      'lastname': 'Smith',
+      'age': 88,
+      'hair_color': 'Brown'
+    },
+    {
+      'firstname': 'Millard',
+      'lastname': 'Fillmore',
+      'age': 50,
+      'hair_color': 'Brown'
+    },
+    {
+      'firstname': 'Maria',
+      'lastname': 'Smith',
+      'age': 62,
+      'hair_color': 'Brown'
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+      <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
+      {/* <PersonCard peopleInfo={peopleInfo}/> */}
     </div>
   );
 }
